612efbe8ef477a8f03fa6335e2d57b590a0b5fc0
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2019-10-14 13:53:45 +0200

    Znaki specjalne, parametr r

---
+++ 2019-10-09/znaki_specjalne.py
@@ -0,0 +1,19 @@
+"""
+Znak              Znaczenie
+_______________________________
+\n                 nowa linia
+\t                 tabulacja
+\'                  apostrof
+\"                cudzysłów
+\\                  ukośnik
+--------------------------------
+
+r - ignoruje znaki specjalne w wyświetlaniu komendą "print"
+
+print(r"jakiś |n \t \' \" \\ tekst")
+"""
+
+
+print("jakiś tekst \n nowa linia \t tabulator \' \" \\ ")
+print("-"*40)
+print(r"jakiś tekst \n nowa linia \t tabulator \' \" \\ ")
