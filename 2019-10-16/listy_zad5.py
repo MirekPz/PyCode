b33c2e1902bec7a606b16f00a75576dcb4f5c608
MirekPz <mirek@info-tur.pl>
2019-10-16 19:12:16 +0200

    Czwarty dzień

---
+++ 2019-10-16/listy_zad5.py
@@ -0,0 +1,20 @@
+"""
+Utwórz “na sztywno” 2-wymiarową tablicę, tak, by kolejne wiersze zawierały dane osób,
+natomiast w kolumnach będzie znajdować się imię, nazwisko, zawód, np:
+    Dorota, Wellman, dziennikarka
+    Adam, Małysz, skoczek
+    Robert, Lewandowski, piłkarz
+    Krystyna, Janda, aktorka
+Wyświetl w sposób przyjazny dla użytkownika
+"""
+
+tablica = [
+    ["Dorota", "Wellman", "dziennikarka"],
+    ["Adam", "Małysz", "skoczek"],
+    ["Robert", "Lewandowski", "piłkarz"],
+    ["Krystyna", "Janda", "aktorka"]
+]
+for i in range(4):
+    print("\n")
+    for j in range(3):
+        print(tablica[i][j], end=" ")
