9c9c1eb9cfbd66d55cd9f733e5446c083be9bf25
MirekPz <mirek@info-tur.pl>
2019-10-19 07:42:54 +0200

    zadanie 4 - niedokończone

---
+++ 2019-10-14/podsumowanie_zad4_gra_kamien-papier-nozyce.py
@@ -0,0 +1,27 @@
+"""
+4▹ Napisz grę - kamień (k) / papier (p) / nożyce (n).
+        Użytkownik podaje jedną z 3 figur.
+        Komputer losuje jedną z 3 figur.
+        Sprawdź kto wygrał tę rundę.
+        Zmień kod tak, by użytkownik mógł podać liczbę rund.
+        Wygrana jest podawana jako suma wygranych rund komputer vs użytkownik.
+        Zmień tak, by gracz mógł zakończyć grę w dowolnej chwili przez np. hasło ‘koniec’
+"""
+
+# 1. Użytkownik podaje jedną z 3 figur do gry: [K]amień, [P]apier, [N]ożyce
+user_figure = " "
+while True:
+    user_figure = input("Wybierz figurę do gry [K, P, N]: ").upper()
+    if user_figure == "K":
+        print("Wybrano KAMIEŃ.")
+        break
+    elif user_figure == "P":
+        print("Wybrano PAPIER.")
+        break
+    elif user_figure == "N":
+        print("Wybrano NOŻYCE.")
+        break
+    else:
+        print("Niepoprawny wybór figury!")
+
+# 2. Komputer losuje jedną z 3 figur
