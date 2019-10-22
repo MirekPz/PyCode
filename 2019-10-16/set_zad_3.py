cdca7beab434bf57f13db9d3610a0023bbc357e6
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2019-10-22 09:06:09 +0200

    Zadanie nr 3 - krotki

--- 2019-10-16/set_zad_3.py
+++ 2019-10-16/set_zad_3.py
@@ -4,3 +4,17 @@ Następnie utwórz listę będącą połączeniem elementów o parzystym indeksi
 oraz nieparzystych elementów z drugiej.
 """
 
+list1 = []
+for i in range(26):
+    list1.append(i)
+tuple1 = tuple(list1)
+
+list2 = []
+for i in range(26):
+    list2.append(chr(97 + i))
+tuple2 = tuple(list2)
+
+print('\nPierwsza krotka:')
+print(tuple1)
+print('\nDruga krotka:')
+print(tuple2)
