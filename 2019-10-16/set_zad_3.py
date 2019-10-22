a98d512710fa297725a2149eb0e79539d746f2bb
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2019-10-22 09:25:35 +0200

    Zadanie nr 3 - krotki parzyste i nieparzyste

--- 2019-10-16/set_zad_3.py
+++ 2019-10-16/set_zad_3.py
@@ -18,3 +18,17 @@ print('\nPierwsza krotka:')
 print(tuple1)
 print('\nDruga krotka:')
 print(tuple2)
+
+# list3 - parzyste indeksy z tuple1 oraz nieparzyste z tuple2
+
+list3 = []
+for i in range(26):
+    if i % 2 == 0:
+        # parzyste z tupli pierwszej
+        list3.append(tuple1[i])
+    else:
+        # nieparzyste z tupli drugiej
+        list3.append(tuple2[i])
+
+print("\nLista po połączeniu elementów o parzystym indeksie z pierwszej krotki oraz elementów o nieparzystym z drugiej:")
+print(list3)
