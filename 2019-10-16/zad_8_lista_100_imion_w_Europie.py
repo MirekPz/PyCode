17bf56dec243ae94953b28fa5598c50f07e9332d
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2019-10-23 13:59:00 +0200

    Zadanie 8 - do zrobienia...

---
+++ 2019-10-16/zad_8_lista_100_imion_w_Europie.py
@@ -0,0 +1,8 @@
+"""
+Utwórz słownik dla 10 krajów Europy zawierający listy 10 najpopularniejszych imion żeńskich.
+Zapisz imiona w wersji anglojęzycznej.
+Dodaj wszystkie listy razem.
+Nowa lista powinna zawierać 100 elementów.
+Wyświetl tylko te imiona, które wystąpiły co najmniej w 3 krajach.
+"""
+
