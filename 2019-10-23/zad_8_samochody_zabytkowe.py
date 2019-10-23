f3f0ba9c4914410c8210b558653c154dbe7261cb
MirekPz <mirek@info-tur.pl>
2019-10-23 20:04:47 +0200

    dokończyć..,

---
+++ 2019-10-23/zad_8_samochody_zabytkowe.py
@@ -0,0 +1,29 @@
+"""
+ Napisz program, który będzie sprawdzał, czy nasz samochód kwalifikuje się do zarejestrowania jako zabytek.
+    Program zacznie ze stworzonym słownikiem o trzech kluczach:
+            marka (str)
+            model (str)
+            rocznik (int)
+    Wypisze ten słownik na ekran (bez żadnego formatowania)
+    Sprawdzi, czy samochód ma minimum 25 lat. Jeśli tak, wypisze komunikat:
+        “Gratulacje! Twój samochód (tutaj_marka) może być zarejestrowany jako zabytek.”
+    Jeśli nie spełnia powyższego warunku, wypisze komunikat:
+        “Twój samochód (tutaj_marka) jest jeszcze zbyt młody.”
+    Gdy program będzie poprawnie działał, pozmieniaj wartości słownika (ale nie klucze!),
+    aby zobaczyć, czy program również zmienia swoje zachowanie.
+"""
+
+cars = {
+        'marka': 'Fiat',
+        'model': 'Seicento',
+        'rocznik': 1999
+        }
+
+print(cars)
+
+car_age = 2019 - cars['rocznik']
+
+if car_age >= 25:
+    print(f"Gratulacje! Twój samochód {cars['marka'] + ' ' + cars['model']} może być zarejestrowany jako zabytek.")
+else:
+    print(f"Twój samochód {cars['marka'] + ' ' + cars['model']} jest jeszcze zbyt młody.")
