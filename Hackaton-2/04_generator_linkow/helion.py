6d9a51831cdabe489409bca9ed9a586d67cd04e4
MirekPz <mirek@info-tur.pl>
2019-11-16 17:40:50 +0100

    generator linków - c.d.

--- Hackaton-2/04_generator_linkow/helion.py
+++ Hackaton-2/04_generator_linkow/helion.py
@@ -7,15 +7,53 @@ while not is_number:
     except ValueError:
         print("\nIndeks musi być liczbą naturalną! Popraw wpisywane dane.")
 
-"""
-Wyświetlenie strony głównej:
-input: https://helion.pl/ ; input: numer partnera - 90412 ; output: https://helion.pl/view/90412
-"""
 
-main_url = "https://helion.pl/"
-output_url = main_url + "view/" + partner_id
-print("\nStrona główna:")
-print(output_url + "\n")
+# rozpoznawanie rodzaju strony na podstawie podanego linku:
+# 1.strona główna     https://helion.pl/
+# 2.tytuł książki np. https://helion.pl/ksiazki/python-wprowadzenie-wydanie-iv-mark-lutz,pytho4.htm#format/d
+# 3.kategoria     np. https://helion.pl/kategorie/elektronika
+
+url = input("Przekopiuj tu link z tytułem książki lub kategorii, dodaj spację na końcu: ")
+
+# jeżeli w nazwie mamy przecinek, to link prowadzi do tytułu konkretnej książki,
+# w przeciwnym wypadku do kategorii lub strony głównej Helionu
+# funkcja find() zwraca -1 jeżeli szukany ciąg nie znajduje się w napisie
+
+comma_position = url.find(",")
+# print(comma_position)
+
+if comma_position > -1:
+    # link jest tytułem książki
+
+    # usuwanie z linka wszystkiego co jest przed przecinkiem:
+    # print(url[url.index(","):])
+    title_symbol = url[url.index(","):]
+
+    # bez początkowego przecinka i do pierwszego wystąpienia kropki w dalszej części linka:
+    title_symbol = title_symbol[1:title_symbol.index(".")]
+    # print(title_symbol)
+
+    output_url = "https://helion.pl/view/" + partner_id + "/" + title_symbol
+    print("Link partnerski do wybranej książki: " + output_url)
+
+elif url == "https://helion.pl/" or url == "helion.pl":
+    """
+    Wyświetlenie strony głównej:
+    input: https://helion.pl/
+    input: numer partnera - 90412
+    output: https://helion.pl/view/90412
+    """
+    main_url = "https://helion.pl/"
+    output_url = main_url + "view/" + partner_id
+    print("\nStrona główna:")
+    print(output_url + "\n")
+
+else:
+    # strona kategorii
+    pass
+
+
+
 
 """
 Wyświetlenie strony produktu:
@@ -23,21 +61,16 @@ input: https://helion.pl/ksiazki/algorytmy-ilustrowany-przewodnik-aditya-bhargav
 output: https://helion.pl/view/90412/algoip
 """
 
-title_url = input("Przekopiuj tu link z tytułem książki, dodaj spacje na końcu: \n")
 
-# usuwanie z linka wszystkiego co jest przed przecinkiem:
-# print(title_url[title_url.index(","):])
-title_symbol = title_url[title_url.index(","):]
 
-# bez początkowego przecinka i do pierwszego wystąpienia kropki w dalszej części linka:
-title_symbol = title_symbol[1:title_symbol.index(".")]
-# print(title_symbol)
-
-output_url = "https://helion.pl/view/" + partner_id + "/" + title_symbol
-print("Link partnerski do wybranej książki: " + output_url)
 
 """
-Wyświetlenie strony promocji:
-input: https://helion.pl/kategorie/promocja-2za1
-output: https://helion.pl/page/90412/promocja/promocja-2za1
+Wyświetlenie linku do kategorii:
+wzór: https://helion.pl/page/id/kategorie/link_do_kategorii
+input: https://helion.pl/kategorie/programowanie
+output: https://helion.pl/page/90412/kategorie/programowanie
 """
+
+#  ?????????/
+#  category_url = input("Przekopiuj tu link z tytułem kategorii, dodaj spacje na końcu: \n")
+
