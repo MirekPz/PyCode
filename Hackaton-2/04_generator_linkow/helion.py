22ffaba5c9565ab49f6ac7a8470b795114dda007
MirekPz <mirek@info-tur.pl>
2019-11-19 03:48:51 +0100

    kontynuacja...

--- Hackaton-2/04_generator_linkow/helion.py
+++ Hackaton-2/04_generator_linkow/helion.py
@@ -7,11 +7,12 @@ while not is_number:
     except ValueError:
         print("\nIndeks musi być liczbą naturalną! Popraw wpisywane dane.")
 
-
-# rozpoznawanie rodzaju strony na podstawie podanego linku:
-# 1.strona główna     https://helion.pl/
-# 2.tytuł książki np. https://helion.pl/ksiazki/python-wprowadzenie-wydanie-iv-mark-lutz,pytho4.htm#format/d
-# 3.kategoria     np. https://helion.pl/kategorie/elektronika
+"""
+rozpoznawanie rodzaju strony na podstawie podanego linku:
+1.strona główna       https://helion.pl/
+2.tytuł książki  np.  https://helion.pl/ksiazki/python-wprowadzenie-wydanie-iv-mark-lutz,pytho4.htm#format/d
+3.kategoria      np.  https://helion.pl/kategorie/elektronika
+"""
 
 url = input("Przekopiuj tu link z tytułem książki lub kategorii, dodaj spację na końcu: ")
 
@@ -36,21 +37,21 @@ if comma_position > -1:
     output_url = "https://helion.pl/view/" + partner_id + "/" + title_symbol
     print("Link partnerski do wybranej książki: " + output_url)
 
-elif url == "https://helion.pl/" or url == "helion.pl":
-    """
-    Wyświetlenie strony głównej:
-    input: https://helion.pl/
-    input: numer partnera - 90412
-    output: https://helion.pl/view/90412
-    """
-    main_url = "https://helion.pl/"
-    output_url = main_url + "view/" + partner_id
-    print("\nStrona główna:")
-    print(output_url + "\n")
-
 else:
-    # strona kategorii
-    pass
+    if url.strip().lower() == "https://helion.pl/" or url.strip().lower() == "helion.pl":
+        """
+            Wyświetlenie strony głównej:
+            input: https://helion.pl/
+            input: numer partnera - 90412
+            output: https://helion.pl/view/90412
+        """
+        main_url = "https://helion.pl/"
+        output_url = main_url + "view/" + partner_id
+        print("\nStrona główna:")
+        print(output_url + "\n")
+    else:
+        # strona kategorii
+        pass
 
 
 
