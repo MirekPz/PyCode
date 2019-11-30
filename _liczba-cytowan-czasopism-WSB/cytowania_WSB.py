ec12c93799f721de520e1d9e8015d1a6f17dc2c8
MirekPz <mirek@info-tur.pl>
2019-11-30 14:14:39 +0100

    Zliczanie cytowan czasopism WSB

--- _liczba-cytowan-czasopism-WSB/cytowania_WSB.py
+++ _liczba-cytowan-czasopism-WSB/cytowania_WSB.py
@@ -1,11 +1,16 @@
 import scholarly
 
-i = 0
-
 search_query = scholarly.search_pubs_query('Studia Periegetica')
 
+i = 0
+
 while not search_query == "":
-    print(next(search_query))
+    try:
+        x = next(search_query)
+    except Exception as e:
+        print("Koniec wyszukiwania", e)
+        break
+    print(x)
     i += 1
     print("Numer kolejny cytowanej pozycji: ", i)
 
